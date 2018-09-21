e501ad7d90de74c359ad39fd34ab9b74e62968a8
MacAttack79 <jmcintosh1979@gmail.com>
2018-09-21 16:55:55 -0400

    updated some code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,44 @@
+var timer = 60;
+var intervalID;
+var correct = 0;
+var incorrect = 10;
+
+function run() {
+  clearInterval(intervalID);
+  intervalID = setInterval(decrement, 1000);
+}
+
+function decrement() {
+  timer--;
+  $("#timer").html(" " + timer + " seconds");
+  if (timer === 0) {
+    stop();
+    $(".trivia-game").hide();
+    $(".score").show();
+  }
+}
+
+function stop() {
+  clearInterval(intervalID);
+  timer = 60;
+  correct = 0;
+  incorrect = 10;
+  // $("input") = empty();
+}
+
+function checkAnswers() {
+  for (var i = 0; i < $("input:checked").length; i++) {
+    if ($("input:checked").val("correct")) {
+      correct++;
+    } 
+    else {
+      incorrect--;
+    }
+    $("#correct").text("You answered " + correct + " questions correctly");
+    $("#incorrect").text("You answered " + incorrect + " questions incorrectly");
+  }
+}
+
 $(document).ready(function() {
   // console.log("document is ready");
 
@@ -8,20 +49,23 @@ $(document).ready(function() {
     // console.log("start button has been clicked");
     $(".welcome").hide();
     $(".trivia-game").show();
+    run();
+    decrement();
   })
 
   $(".submit").on("click", function() {
-    // console.log("submit button has been clicked");
-    $(".welcome").hide();
+    stop();
     $(".trivia-game").hide();
     $(".score").show();
-  })
+    checkAnswers();
+})
 
   $(".re-play").on("click", function() {
     // console.log("re-play button has been clicked");
+
     $(".welcome").show();
-    $(".trivia-game").hide();
     $(".score").hide();
+    run();
   })
 
 })
